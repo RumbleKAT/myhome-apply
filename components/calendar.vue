abd239c56f158d8c8c14e04d553a7a2778c26e7f
Rumblekat <reki318@naver.com>
2022-06-04 14:13:09 +0900

    [feat] adjust v-calendar

---
+++ components/calendar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Calendar } from 'v-calendar';
+</script>
+
+<template>
+  <div>
+    <ClientOnly><Calendar /></ClientOnly>
+  </div>
+</template>
+<script lang="ts"> 
+console.log("loaded..")
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,18 @@
 <div>
     <h1>template page</h1>
     <p>ksdksjdksjdksjkdjskdjdlskdlskdls</p>
+    <Calendar></Calendar>
 </div>
 </template>
 
 <script>
+import Calendar from '../components/calendar.vue'
+
 export default {
   name: "IndexPage",
+  components : {
+    Calendar
+  },
 };
 </script>
 
