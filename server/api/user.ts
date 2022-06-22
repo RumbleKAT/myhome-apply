f6f58543590c93f8868ab33b762a8a8c1c44f0ce
RumbleKAT <reki318@naver.com>
2022-06-22 23:32:23 +0900

    [feat] add getUserByMail

--- server/api/user.ts
+++ server/api/user.ts
@@ -1,6 +1,8 @@
-import { createApp, createRouter } from 'h3'
+import { createApp, createRouter,useBody,createError,sendError } from 'h3'
 import cors from 'cors';
-import { execute } from '../../server/utils/Repository';
+import { selectByEmail } from '../service/userService';
+import { IHomeInfo } from '../model/IHomeInfo';
+import { emailValid } from '../utils/Validation';
 
 const app = createApp()
 const router = createRouter()
@@ -8,8 +10,14 @@ const router = createRouter()
 app.use(cors());
 app.use(router)
 
-router.get('/',async()=>{
-    const res = await execute('select * from home_info',[])
+
+router.post('/getUserByMail',async(req,next)=>{
+    const body:IHomeInfo = await useBody(req);
+    
+    if(!emailValid(body.email)) 
+        sendError(req, createError({statusCode: 400, statusMessage: 'Email Validation Error'}));
+    
+    const res = await selectByEmail(body); 
     return { "data" : res } ;
 });
 
