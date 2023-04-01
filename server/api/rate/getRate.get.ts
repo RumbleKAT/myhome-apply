21849e4508c3a63a79a79aa4e2748e2dcec572ef
Rumblekat <reki318@naver.com>
2023-04-01 23:58:22 +0900

    (feat) getRate info api

---
+++ server/api/rate/getRate.get.ts
@@ -0,0 +1,49 @@
+import {defineEventHandler} from "h3";
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const url = 'https://www.hf.go.kr/ko/sub02/sub02_01_07_01.do'
+export default defineEventHandler(async (event) => {
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data);
+    let period:string = "";
+
+    $('.taR.font16.mgt30').each(function (i, elem) {
+        // Extract the text from the element
+        period = $(this).text().trim();
+    });
+    let banks:any[] = [];
+    $('.tbl tbody').each(function (i, elem) {
+        // Iterate through each tr within the tbody
+        $(this).find('tr').each(function (j, trElem) {
+            // Extract the data within the tr
+            const rowData:any = [];
+            $(trElem).find('td').each(function (k, tdElem) {
+                rowData.push($(tdElem).text().trim());
+            });
+
+            // rowData now contains the three td values for the current tr
+            console.log(`Row ${j + 1}:`, rowData);
+            banks.push(rowData);
+        });
+    });
+
+    try{
+        const bank = banks.map(p=>{
+            return {
+                "name" : p[0],
+                "rate" : p[1],
+                "call" : p[2]
+            }
+        });
+        return {
+            period,
+            bank
+        }
+    }catch (e) {
+        console.log(e);
+        return {
+            "message" : "Not Found"
+        }
+    }
+});
