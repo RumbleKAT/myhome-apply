fa16e9ee4f785f8f31839e70a9f88fa157ee56fa
Song Myeong Jin <reki318@naver.com>
2025-06-05 22:18:36 +0900

    Specify default dev server port

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  nitro: {
+    devServer: {
+      port: parseInt(process.env.PORT || process.env.NUXT_PORT || '3000')
+    }
+  }
+})
